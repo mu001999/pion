72c7bb9034b52c6cfd2f873378e356bf810ab2fc
mu001999 <mu001999@outlook.com>
2017-08-12 17:47:03 +0800

    change

---
+++ pion.py
@@ -0,0 +1,295 @@
+import wsgiref
+import sys
+import re
+import threading
+import cgi
+from http.cookies import SimpleCookie
+
+
+class ServerAdapter(object):
+    def __init__(self, host='127.0.0.1', port=8000, **kargs):
+        self.host = host
+        self.port = int(port)
+        self.options = kargs
+    
+    def __repr__(self):
+        return "{} ({}:{})".format(self.__class__.__name__, self.host, self.port)
+
+    def run(self, handler):
+        pass
+
+
+class WSGIRefServer(ServerAdapter):
+    def run(self, handler):
+        from wsgiref.simple_server import make_server
+        server = make_server(self.host, self.port, handler)
+        server.serve_forever()
+
+
+def WSGIHandler(environ, start_response):
+    """The bottle WSGI-handler."""
+    global request
+    global response
+    request.bind(environ)
+    response.bind()
+    try:
+        handler, args = match_url(request.path, request.method)
+        if not handler:
+            raise HTTPError(404, "Not found")
+        output = handler(**args)
+    except:
+        raise BaseException
+
+    if hasattr(output, 'read'):
+        fileoutput = output
+        if 'wsgi.file_wrapper' in environ:
+            output = environ['wsgi.file_wrapper'](fileoutput)
+        else:
+            output = iter(lambda: fileoutput.read(8192), '')
+    elif isinstance(output, str):
+        output = [output]
+
+    for c in response.COOKIES.values():
+        response.header.add('Set-Cookie', c.OutputString())
+
+    # finish
+    status = '%d %s' % (response.status, HTTP_CODES[response.status])
+    start_response(status, list(response.header.items()))
+    return output
+
+
+class Request(threading.local):
+    def bind(self, environ):
+        self._environ = environ
+        self._GET = None
+        self._POST = None
+        self._GETPOST = None
+        self._COOKIES = None
+        self.path = self._environ.get('PATH_INFO', '/').strip()
+        if not self.path.startswith('/'):
+            self.path = '/' + slef.path
+    
+    @property
+    def method(self):
+        return self._environ.get('REQUEST_METHOD', 'GET').upper()
+
+    @property
+    def query_string(self):
+        return self._environ.get('QUERY_STRING', '')
+
+    @property
+    def input_length(self):
+        try:
+            return int(self._environ.get('CONTENT_LENGTH', '0'))
+        except ValueError:
+            return 0
+
+    @property
+    def GET(self):
+        if self._GET is None:
+            raw_dict = parse_qs(self.query_string, keep_blank_values=1)
+            self._GET = {}
+            for key, value in raw_dict.items():
+                if len(value) == 1:
+                    self._GET[key] = value[0]
+                else:
+                    self._GET[KEY] = value
+        return self._GET
+
+    @property
+    def POST(self):
+        if self._POST is None:
+            raw_data = cgi.FieldStorage(
+                fp=self._environ['wsgi.input'], 
+                environ=self._environ
+            )
+            self._POST = {}
+            if raw_data:
+                for key in raw_data:
+                    if isinstance(raw_data[key], list):
+                        self._POST[key] = [v.value for v in raw_data[key]]
+                    elif raw_data[key].filename:
+                        self._POST[key] = raw_data[key]
+                    else:
+                        self._POST[key] = raw_data[key].value
+        return self._POST
+
+    @property
+    def params(self):
+        if self._GETPOST is None:
+            self._GETPOST = dict(self.GET)
+            self._GETPOST.update(dict(self.POST))
+        return self._GETPOST
+
+    @property
+    def COOKIES(self):
+        if self._COOKIES is None:
+            raw_dict = SimpleCookie(self._environ.get('HTTP_COOKIE', ''))
+            self._COOKIES = {}
+            for cookie in raw_dict.values():
+                self._COOKIES[cookie.key] = cookie.value
+        return self._COOKIES
+
+
+class HeaderDict(dict):
+    def __setitem__(self, key, value):
+        return dict.__setitem__(self, key.title(), value)
+
+    def __getitem__(self, key):
+        return dict.__getitem__(self, key.title())
+
+    def __delitem__(self, key):
+        return dict.__delitem__(self, key.title())
+
+    def __contains__(self, key):
+        return dict.__contains__(self, key.titles())
+
+    def items(self):
+        for key, values in dict.items(self):
+            if not isinstance(values, list):
+                values = [values]
+            for value in values:
+                yield (key, str(value))
+    
+    def add(self, key, value):
+        if isinstance(value, list):
+            for v in value:
+                self.add(key, v)
+        elif key in self:
+            if isinstance(self[key], list):
+                self[key].append(value)
+            else:
+                self[key] = [self[key], value]
+        else:
+            self[key] = [value]
+
+
+class Response(threading.local):
+    def bind(self):
+        self._COOKIES = None
+        self.status = 200
+        self.header = HeaderDict()
+        self.content_type = 'text/html'
+        self.error = None
+
+    @property
+    def COOKIES(self):
+        if not self._COOKIES:
+            self._COOKIES = Cookie.SimpleCookie()
+        return self._COOKIES
+
+    def set_cookie(self, key, value, **kargs):
+        self.COOKIES[key] = value
+        for k in kargs:
+            self.COOKIES[key][k] = kargs[k]
+
+    def get_content_type(self):
+        return self.header['Content-Type']
+
+    def set_content_type(self, value):
+        self.header['Content-Type'] = value
+
+    content_type = property(
+        get_content_type,
+        set_content_type,
+        None,
+        get_content_type.__doc__
+    )
+
+
+def match_url(url, method='GET'):
+    url = '/' + url.strip().lstrip("/")
+    route = ROUTES_SIMPLE.get(method,{}).get(url,None)
+    if route:
+      return (route, {})
+    
+    routes = ROUTES_REGEXP.get(method,[])
+    for i in xrange(len(routes)):
+        match = routes[i][0].match(url)
+        if match:
+            handler = routes[i][1]
+            if i > 0 and OPTIMIZER and random.random() <= 0.001:
+              routes[i-1], routes[i] = routes[i], routes[i-1]
+            return (handler, match.groupdict())
+    return (None, None)
+
+
+def compile_route(route):
+    route = route.strip().lstrip('$^/ ').rstrip('$^ ')
+    route = re.sub(r':([a-zA-Z_]+)(?P<uniq>[^\w/])(?P<re>.+?)(?P=uniq)', r'(?P<\1>\g<re>)', route)
+    route = re.sub(r':([a-zA-Z_]+)', r'(?P<\1>[^/]+)', route)
+    return re.compile('^/%s$' % route)
+
+
+def add_route(route, handler, method='GET', simple=False):
+    method = method.strip().upper()
+    if re.match(r'^/(\w+/)*\w*$', route) or simple:
+        ROUTES_SIMPLE.setdefault(method, {})[route] = handler
+    else:
+        route = compile_route(route)
+        ROUTES_REGEXP.setdefault(method, []).append([route, handler])
+        
+
+def route(url, **kargs):
+    def wrapper(handler):
+        add_route(url, handler, **kargs)
+        return handler
+    return wrapper
+
+
+def run(server=WSGIRefServer, host='127.0.0.1', port=8000, optinmize=False, **kargs):
+    server = server(host=host, port=port, **kargs)
+    server.run(WSGIHandler)
+
+
+DEBUG = False
+OPTIMIZER = False
+ROUTES_SIMPLE = {}
+ROUTES_REGEXP = {}
+ERROR_HANDLER = {}
+HTTP_CODES = {
+    100: 'CONTINUE',
+    101: 'SWITCHING PROTOCOLS',
+    200: 'OK',
+    201: 'CREATED',
+    202: 'ACCEPTED',
+    203: 'NON-AUTHORITATIVE INFORMATION',
+    204: 'NO CONTENT',
+    205: 'RESET CONTENT',
+    206: 'PARTIAL CONTENT',
+    300: 'MULTIPLE CHOICES',
+    301: 'MOVED PERMANENTLY',
+    302: 'FOUND',
+    303: 'SEE OTHER',
+    304: 'NOT MODIFIED',
+    305: 'USE PROXY',
+    306: 'RESERVED',
+    307: 'TEMPORARY REDIRECT',
+    400: 'BAD REQUEST',
+    401: 'UNAUTHORIZED',
+    402: 'PAYMENT REQUIRED',
+    403: 'FORBIDDEN',
+    404: 'NOT FOUND',
+    405: 'METHOD NOT ALLOWED',
+    406: 'NOT ACCEPTABLE',
+    407: 'PROXY AUTHENTICATION REQUIRED',
+    408: 'REQUEST TIMEOUT',
+    409: 'CONFLICT',
+    410: 'GONE',
+    411: 'LENGTH REQUIRED',
+    412: 'PRECONDITION FAILED',
+    413: 'REQUEST ENTITY TOO LARGE',
+    414: 'REQUEST-URI TOO LONG',
+    415: 'UNSUPPORTED MEDIA TYPE',
+    416: 'REQUESTED RANGE NOT SATISFIABLE',
+    417: 'EXPECTATION FAILED',
+    500: 'INTERNAL SERVER ERROR',
+    501: 'NOT IMPLEMENTED',
+    502: 'BAD GATEWAY',
+    503: 'SERVICE UNAVAILABLE',
+    504: 'GATEWAY TIMEOUT',
+    505: 'HTTP VERSION NOT SUPPORTED',
+}
+
+request = Request()
+response = Response()
--- pion/__init__.py
+++
@@ -1,5 +0,0 @@
-from pion.app import run
-from pion.router import route
-
-__author__ = 'IceBo'
-__version__ = '0.0.0.1'
